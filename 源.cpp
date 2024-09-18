155ca0e3c60bbc27f1247e564f7d75fbf3c7f765
050209lty <2560694630@qq.com>
2024-09-19 00:23:13 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+//typedef unsigned int u_int;
+//int main()
+//	unsigned int num = 100;
+//	u_int num2 = 100;
+//
+//	return 0;
+//}
+//typedef:类型重定义
+
+
+//static静态的
+//{
+//1  修饰局部变量
+////2  修饰全局变量
+////3  修饰函数
+// void test()
+//{
+//	static int a = 1;
+//	a++;
+//	printf("%d",a);
+//
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	while (a < 10)
+//	{
+//		test();
+//		a++;
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	int g_val = 10;
+	printf("%d\n",g_val);
+
+	return 0;
+}
